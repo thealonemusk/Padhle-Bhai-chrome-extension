daa42ae34b3002fdec95c68e35ae75b3ef056338
Ashutosh JHa <ashutoshj.cs.21@nitj.ac.in>
2023-03-12 22:46:37 +0530

    chs

--- Padhle Bhai/blocker.js
+++ Padhle Bhai/blocker.js
@@ -21,7 +21,7 @@ const defineCSS = () => {
     </div>`
   }
   
-  const forbiddenURLs = ["twitter.com", "web.whatsapp.com","instagram.com","netflix.com","pinterest.com","reddit.com","amazon.com","music.youtube.com","facebook.com"];
+  const forbiddenURLs = ["instagram.com","twitter.com", "web.whatsapp.com","instagram.com","netflix.com","pinterest.com","reddit.com","amazon.com","music.youtube.com","facebook.com"];
   const set = new Set(forbiddenURLs);
   
   if (set.has(window.location.hostname)) {
